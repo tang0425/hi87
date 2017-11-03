88844d16c83ceca2d76babaf35784cc19e87757f
tang6967 <powerlife0983@gmail.com>
2017-11-03 14:38:32 +0800

    hi1103

---
+++ httpd/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+ let http = require('http');
+
+ http.createServer((request, response) => {
+   request.on('end', () => {
+     console.log('Request method: ' + request.method);
+     console.log('Request url: ' + request.url);
+     });
+
+  response.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+
+  // 傳送回應內容。
+  response.end('Hello World!\n');
+  }).listen(8088);
+  
+
+// log message to Console
+console.log(' Server running at  http://127.0.0.1:8088/');
